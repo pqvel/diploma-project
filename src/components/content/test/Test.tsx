88c8d50ac9c144ea887e98a678b5b8e52bb22794
Pavel Salianik <76737719+pqvel@users.noreply.github.com>
2024-02-05 12:32:18 +0300

    wip test

--- src/components/content/test/Test.tsx
+++ src/components/content/test/Test.tsx
@@ -1,4 +1,7 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import { Button, Form, Typography } from "antd"
+
+const { Title, Paragraph } = Typography
 
 const enum TestInputType {
     radio = "radio",
@@ -8,25 +11,38 @@ const enum TestInputType {
 type Variant = {
     id: string
     title: string
+    isSelect: boolean
 }
 
 type Props = {
     title: string
     descr: string
+    variants: Variant[]
+    rightAnswers: string[]
 }
 
-const Test:FC<Props> = ({ title, descr }) => {
-    const rightAnswers = []
-    const variants: Variant[] = []
+const Test:FC<Props> = ({ title, descr, variants, rightAnswers }) => {
     const inputType: TestInputType = rightAnswers.length === 1 ? TestInputType.radio : TestInputType.checkbox
+    const [selectedVariants, selectedVariants]
+    
+    // return <div>
+    //     <h3>{title}</h3>
+    //     <p>{descr}</p>
+    //     <div>
+    //         {variants.map(variant => <TestInput title={variant.title} type={inputType} key={variant.title}/>)}
+    //     </div>
+    //     <Button type="primary">Проверить</Button>
+    // </div>
+    return (
 
-    return <div>
-        <h3>{title}</h3>
-        <p>{descr}</p>
-        <div>
-            {variants.map(variant => <TestInput title={variant.title} type={inputType} key={variant.title}/>)}
-        </div>
-    </div>
+        <Form>
+            <Typography>
+                <Title level={3}>{title}</Title>
+                <Paragraph>{descr}</Paragraph>
+            </Typography>
+            
+        </Form>
+    )
 }
 
 type TestInputProps = {
@@ -34,9 +50,11 @@ type TestInputProps = {
     type: TestInputType
 }
 
-const TestInput: FC<TestInputProps> = ({type, title}) => (
-    <label>
-        <input type={type}/>
-        {title}
-    </label>
+const TestInput: FC<TestInputProps> = ({type, title, isSelect}) => (
+    <>
+        <label className={isSelect ? "" : ""}>
+            <input type={type}/>
+            {title}
+        </label>
+    </>
 )
