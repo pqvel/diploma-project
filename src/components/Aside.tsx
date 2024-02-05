cf5e2c8fc59a34fff3392f76975a89fb71d33642
Pavel Salianik <solyqnik@gmail.com>
2024-02-05 07:28:43 +0300

    wip

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,67 +1,90 @@
 import { FC } from "react";
 import { v4 as uuid } from "uuid";
-import { slugify } from "../../utils/slug";
-import Link from "next/link";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
 
-type Link = {
-	id: string;
-	title: string;
-	href: string;
-};
+type MenuItem = Required<MenuProps>["items"][number];
 
-type Block = {
-	title: string;
-	id: string;
-	links: Array<Link>;
-};
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
 
-type Links = Array<Block>;
+const items: MenuProps["items"] = [
+  getItem("Navigation One", "sub1", <MailOutlined />, [
+    getItem(
+      "Item 1",
+      "g1",
+      null,
+      [getItem("Option 1", "1"), getItem("Option 2", "2")],
+      "group"
+    ),
+    getItem(
+      "Item 2",
+      "g2",
+      null,
+      [getItem("Option 3", "3"), getItem("Option 4", "4")],
+      "group"
+    ),
+  ]),
 
-const links: Links = [
-	{
-		title: "Глава 1",
-		id: uuid(),
-		links: [
-			{
-				id: uuid(),
-				title: "Основы Java Script",
-				href: "/" + slugify("Основы Java Script"),
-			},
-			{
-				id: uuid(),
-				title: "Переменные java script",
-				href: "/" + slugify("Переменные java script"),
-			},
-			{
-				id: uuid(),
-				title: "Типы в Java Script",
-				href: "/" + slugify("Типы в Java Script"),
-			},
-		],
-	},
+  getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
+    getItem("Option 5", "5"),
+    getItem("Option 6", "6"),
+    getItem("Submenu", "sub3", null, [
+      getItem("Option 7", "7"),
+      getItem("Option 8", "8"),
+    ]),
+  ]),
+
+  { type: "divider" },
+
+  getItem("Navigation Three", "sub4", <SettingOutlined />, [
+    getItem("Option 9", "9"),
+    getItem("Option 10", "10"),
+    getItem("Option 11", "11"),
+    getItem("Option 12", "12"),
+  ]),
+
+  getItem(
+    "Group",
+    "grp",
+    null,
+    [getItem("Option 13", "13"), getItem("Option 14", "14")],
+    "group"
+  ),
 ];
 
 const Aside: FC = () => {
-	return (
-		<aside>
-			<nav>
-				{links.map((block) => {
-					return (
-						<div key={block.id}>
-							<h5>{block.title}</h5>
-							{block.links.map((link) => {
-								return (
-									<Link key={link.id} href={`/lessons/${link.href}`}>
-										{link.title}
-									</Link>
-								);
-							})}
-						</div>
-					);
-				})}
-			</nav>
-		</aside>
-	);
+  return (
+    <aside>
+      <Menu
+        mode="inline"
+        items={[
+          {
+            label: "Глава 1, основы js",
+			key: 
+          },
+        ]}
+      ></Menu>
+    </aside>
+  );
 };
 
 export default Aside;
