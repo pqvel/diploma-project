4e9382572a82605c7334d4113c27e40da0aaf391
Pavel Salianik <solyqnik@gmail.com>
2024-06-10 19:35:23 +0300

    +

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.scss";
+import NextTopLoader from "nextjs-toploader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader showSpinner={false} color="black" />
         <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
