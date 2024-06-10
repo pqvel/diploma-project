facdb1d3b3f3f48755e99b4c3e0656dbff2f0b18
Pavel Salianik <solyqnik@gmail.com>
2024-06-10 19:23:31 +0300

    +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,32 @@ import { FC } from "react";
 import MainLayout from "@/components/layouts/MainLayout";
 
 const MainPage: FC = async () => {
-  return <MainLayout>pag</MainLayout>;
+  return (
+    <MainLayout>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white border border-gray-300 rounded-lg p-6 text-center">
+          <p className="text-lg text-gray-700">
+            Занимайтесь на нашей платформе
+          </p>
+        </div>
+        <div className="bg-white border border-gray-300 rounded-lg p-6 text-center">
+          <p className="text-lg text-gray-700">
+            Развивайте практические навыки
+          </p>
+        </div>
+        <div className="bg-white border border-gray-300 rounded-lg p-6 text-center">
+          <p className="text-lg text-gray-700">
+            Практикуйте и совершенствуйтесь
+          </p>
+        </div>
+        <div className="bg-white border border-gray-300 rounded-lg p-6 text-center">
+          <p className="text-lg text-gray-700">
+            Исследуйте новые возможности с нами
+          </p>
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default MainPage;
